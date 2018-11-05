e2dca9708e510de443a3ea46b5d9fa269380be72
jonathanloganmoran <jmoran23@ucmerced.edu>
2018-11-04 21:22:27 -0800

    Refactored  class to extend comparator and  object type

--- threads/Alarm.java
+++ threads/Alarm.java
@@ -3,8 +3,6 @@ package nachos.threads;
 import java.util.LinkedList;
 
 import nachos.machine.*;
-import nachos.threads.Lock;		// NEW waitLock - P1 T1.3
-import java.util.Iterator;
 
 
 /**
@@ -24,34 +22,9 @@ public class Alarm {
 		public void run() { timerInterrupt(); }
 	    });
 	
-	// NEW for T1.3: private class AlarmThread to sort in queue - @jonathanloganmoran 11/4
-	private class AlarmThread implements Comparable<AlarmThread> {
-	    public AlarmThread(KThread thread, long sleepUntil) {
-		this.thread = thread;
-		this.sleepUntil = sleepUntil;		// sort in ascending (FIFO)
-	    }
-	    // NEW for T1.3: extends Comparable to sort threads by dueTime (FIFO)
-	    public int compareTo(AlarmThread alarmThread) {
-		if(this.sleepUntil > alarmThread.sleepUntil) {
-		    return 1; 				// due before -> put front
-		}
-		else if(this.sleepUntil < alarmThread.sleepUntil) {
-		    return -1;				// due after -> push back
-		}
-		else {
-		    return 0;				// not due
-		}
-	    }
-	    
-	    //NEW for T1.3: move instance variables - @jonathanmoran 11/4
-	    private KThread thread;			// waitingQueue thread
-	    private long sleepUntil;			// thread dueTime ref
-	}
 	// NEW for T1.3: Remove lock instance -> disable machine interrupts (Issue #3)
 	//waitLock = new Lock();				// init lock for waitUntil()
-	waitingQueue = new LinkedList<AlarmThread>();	// init queue for waiting threads
-	
-	
+	waitingQueue = new LinkedList<AlarmThread>();		// init queue for waiting threads
 	
     }
 
@@ -65,26 +38,27 @@ public class Alarm {
 	// NEW T1.3: check if threads are due on waitingQueue
 	
 	/* NEW T1.3: disable interrupts to place on ready()
-	/*waitLock.acquire();				// grab lock for each thread
+	/*waitLock.acquire();					// grab lock for each thread
 	 */
 	boolean intStatus = Machine.interrupt().disable();
 	
-	// NEW T1.3: check if threads are due on waitingQueue
-	while(!waitingQueue.isEmpty()) {
-	    for(AlarmThread it : waitingQueue) {		// "look" through LinkedList
-		// check dueTime against this CPU clock time and init case == -1
-		if(it.getDueTime() <= Machine.timer().getTime() && it.getDueTime() > -1) {
-		    it.ready();				// flag for readyQueue
-		    waitingQueue.remove(it);		// remove from waitingQueue
-		} 					// else proceed
-	    }						// till all threads are checked
-	} 						// else no waiting threads at currentTime
+	// NEW T1.3: check if threads are due on waitingQueue -> prep for T1.5 PriorityQueue implementation
+	while(!waitingQueue.isEmpty() && waitingQueue.peek().sleepUntil <= Machine.timer().getTime()) {
+	    AlarmThread thread = waitingQueue.remove();
+	    thread.waitingThread.ready();
+	}
 	
-	// NEW T1.3: check if threads are due on waitingQueue
-	waitLock.release();
-	// context switch
-	KThread.yield();
-    }
+	Machine.interrupt().restore(intStatus);
+	    // OLD T1.3: checking without comparator
+//	    for(AlarmThread it : waitingQueue) {		// "look" through LinkedList
+//		// check dueTime against this CPU clock time and init case == -1
+//		if(it.sleepUntil <= Machine.timer().getTime()) {
+//		    thread = waitingQueue.remove(it);		// hold temporarily
+//		    it.ready();					// flag for readyQueue
+//		    waitingQueue.remove(it);			// remove from waitingQueue
+//		} 						// else proceed
+	    	// till all threads are checked
+	} 							// else no waiting threads at currentTime
 
     /**
      * Put the current thread to sleep for at least <i>x</i> ticks,
@@ -102,22 +76,52 @@ public class Alarm {
      */
     public void waitUntil(long x) {
 	
+	boolean intStatus = Machine.interrupt().disable();
 	long dueTime = (Machine.timer().getTime() + x);
 	
-	
-	// NEW T1.3: update thread's due time
-	KThread.currentThread().setDueTime(dueTime);
+	// NEW T1.3: update AlarmThread's due time
+	AlarmThread waitingThread = new AlarmThread(KThread.currentThread(), dueTime);
 	
 	// NEW for Task 1.3: put thread onto waitingQueue
-	waitingQueue.add(KThread.currentThread());
-	waitLock.acquire();
+	waitingQueue.add(waitingThread);
 	KThread.sleep();
-	waitLock.release();
 
+	Machine.interrupt().restore(intStatus);
     }
     
-    // NEW Task 1.3: Alarm Class
-    private Lock waitLock;				// acquire lock in waitUntil()
+    // NEW for T1.3: container class AlarmThread to sort in queue - @jonathanloganmoran 11/4
+    public class AlarmThread implements Comparable<AlarmThread> {
+	
+	// constructor for new AlarmThread priority instance
+	public AlarmThread(KThread waitingThread, long sleepUntil) {
+	    this.waitingThread = waitingThread;
+	    this.sleepUntil = sleepUntil;		// sort in ascending (FIFO)
+	}
+	
+	// NEW for T1.3 -> T1.5: sort threads by dueTime (FIFO) for PriorityQueue
+	public int compareTo(AlarmThread alarmThread) {
+
+	    // NEW T1.5: extends Java Long compareTo, returns effectivePriority for waitingQueue 
+	    return (new Long(this.sleepUntil).compareTo(new Long (alarmThread.sleepUntil)));
+	    
+	    // OLD T1.3: compares this thread's dueTime against waitingQueue instance's dueTime 	    
+//	    if(this.sleepUntil > alarmThread.sleepUntil) {
+//		return 1; 				// due before -> put front
+//	    }
+//	    else if(this.sleepUntil < alarmThread.sleepUntil) {
+//		return -1;				// due after -> push back
+//	    }
+//	    else {
+//		return 0;				// not due
+//	    }
+	}
+	
+	//NEW for T1.3: AlarmThread instance variables - @jonathanmoran 11/4
+	private KThread waitingThread;			// waitingQueue thread
+	private long sleepUntil;			// thread dueTime ref
+    }
+
+    // NEW Task 1.3: Alarm Class - not using PriorityQueue yet
     private LinkedList<AlarmThread> waitingQueue;	// hold waitUntil() threads
 
 }
